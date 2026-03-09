6dea9a43f289aa02dfb78d17a9dce1b1e74ce74f
Polshkrev <105235054+Polshkrev@users.noreply.github.com>
2026-03-09 10:16:15 -0400

    Add -a flag

--- cmd/meta.go
+++ cmd/meta.go
@@ -9,6 +9,7 @@ import (
 	"strings"
 
 	"github.com/Polshkrev/gopolutils"
+	"github.com/Polshkrev/gopolutils/collections/safe"
 	"github.com/Polshkrev/gopolutils/fayl"
 	"github.com/Polshkrev/meta/models"
 )
@@ -18,6 +19,43 @@ const (
 	FileName string      = "meta"
 	FileType fayl.Suffix = fayl.Toml
 )
+
+var (
+	mapping safe.Mapping[string, []string] = safe.NewMap[string, []string]()
+)
+
+func initializeMapping(mapping safe.Mapping[string, []string], programme *models.Programme) {
+	var except *gopolutils.Exception = mapping.Insert("tags", programme.Tags)
+	if except != nil {
+		panic(except)
+	}
+	except = mapping.Insert("contributers", programme.Contributers)
+	if except != nil {
+		panic(except)
+	}
+	except = mapping.Insert("paths", programme.AvailablePaths())
+	if except != nil {
+		panic(except)
+	}
+	except = mapping.Insert("urls", programme.AvailableUrls())
+	if except != nil {
+		panic(except)
+	}
+	except = mapping.Insert("commands", programme.AvailableCommands())
+	if except != nil {
+		panic(except)
+	}
+}
+
+func getAvailableKeys(key string, mapping safe.Mapping[string, []string]) ([]string, *gopolutils.Exception) {
+	var keys *[]string
+	var except *gopolutils.Exception
+	keys, except = mapping.At(key)
+	if except != nil {
+		return nil, except
+	}
+	return *keys, nil
+}
 
 func readMeta(path *fayl.Path) *models.Programme {
 	if !path.Exists() {
@@ -52,7 +90,7 @@ func availableCommands(programme *models.Programme, intent models.Command, comma
 
 func runCommand(programme *models.Programme, intent models.Command, outputChannel chan<- string, errorChannel chan<- error) {
 	if len(intent) == 0 {
-		fmt.Fprintln(os.Stderr, gopolutils.NewException("No arguments have been provided."))
+		fmt.Fprintln(os.Stderr, gopolutils.NewNamedException(gopolutils.RuntimeError, "No arguments have been provided."))
 		flag.Usage()
 		os.Exit(1)
 	}
@@ -65,30 +103,43 @@ func runCommand(programme *models.Programme, intent models.Command, outputChanne
 	go getOutput(cmd, outputChannel, errorChannel)
 }
 
-func main() {
-	var programme *models.Programme = readMeta(fayl.PathFromParts(Folder, FileName, FileType))
-	var versionFlag *bool = flag.Bool("version", false, "Display the version of the programme.")
-	var licenseFlag *bool = flag.Bool("license", false, "Display the license of the programme.")
-	flag.Parse()
-	if *versionFlag {
-		fmt.Printf("%s - %s", programme.Project.Name, programme.Project.Version.ToString())
-		os.Exit(0)
-	}
-	if *licenseFlag {
-		fmt.Print(gopolutils.Must(programme.ReadLicense()))
-		os.Exit(0)
-	}
-	var intent models.Command = cmp.Or(
-		flag.Arg(0),
-		models.Script,
-	)
+func run(programme *models.Programme, intent models.Command) string {
 	var outputChannel chan string = make(chan string, 1)
 	var errorChannel chan error = make(chan error, 1)
 	go runCommand(programme, intent, outputChannel, errorChannel)
 	var output string = <-outputChannel
 	var outputError error = <-errorChannel
 	if outputError != nil {
-		panic(outputError)
+		panic(gopolutils.NewNamedException(gopolutils.IOError, outputError.Error()))
 	}
+	return output
+}
+
+func main() {
+	var programme *models.Programme = readMeta(fayl.PathFromParts(Folder, FileName, FileType))
+	var versionFlag *bool = flag.Bool("version", false, "Display the version of the programme.")
+	var licenseFlag *bool = flag.Bool("license", false, "Display the license of the programme.")
+	var listFlag *string = flag.String("a", "", "Display all available keys at the given label.")
+	flag.Parse()
+	if *versionFlag {
+		fmt.Printf("%s - %s", programme.Project.Name, programme.Project.Version.ToString())
+		os.Exit(0)
+	} else if *licenseFlag {
+		fmt.Print(gopolutils.Must(programme.ReadLicense()))
+		os.Exit(0)
+	} else if *listFlag != "" {
+		initializeMapping(mapping, programme)
+		var keys []string = gopolutils.Must(getAvailableKeys(*listFlag, mapping))
+		var i int
+		for i = range keys {
+			var key string = keys[i]
+			fmt.Println(key)
+		}
+	}
+	var intent models.Command = cmp.Or(
+		flag.Arg(0),
+		models.Script,
+	)
+	var output string = run(programme, intent)
 	fmt.Print(output) // TODO: Make this a redirect instead of simply printing.
 }
