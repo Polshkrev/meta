baed0ed4ffcafe173aa668da585bbf5bcf6b853f
Polshkrev <105235054+Polshkrev@users.noreply.github.com>
2026-03-30 21:36:50 -0400

    Remove locks

--- models/project.go
+++ models/project.go
@@ -1,19 +1,12 @@
 package models
 
-import "sync"
-
 // The meta details of a language-agnostic serializable project.
 type Project struct {
-	nameLock        sync.RWMutex
-	Name            string `json:"name,omitempty,omitzero" yaml:"name,omitempty,omitzero" toml:"name,omitempty,omitzero"`
-	authorLock      sync.RWMutex
-	Author          *Author `json:"author,omitempty,omitzero" yaml:"author,omitempty,omitzero" toml:"author,omitempty,omitzero"`
-	descriptionLock sync.RWMutex
-	Description     string `json:"description,omitempty,omitzero" yaml:"description,omitempty,omitzero" toml:"description,omitempty,omitzero"`
-	typeLock        sync.RWMutex
-	Type            Type `json:"type,omitempty,omitzero" yaml:"type,omitempty,omitzero" toml:"type,omitempty,omitzero"`
-	versionLock     sync.RWMutex
-	Version         *Version `json:"version,omitempty,omitzero" yaml:"version,omitempty,omitzero" toml:"version,omitempty,omitzero"`
+	Name        string   `json:"name,omitempty,omitzero" yaml:"name,omitempty,omitzero" toml:"name,omitempty,omitzero"`
+	Author      *Author  `json:"author,omitempty,omitzero" yaml:"author,omitempty,omitzero" toml:"author,omitempty,omitzero"`
+	Description string   `json:"description,omitempty,omitzero" yaml:"description,omitempty,omitzero" toml:"description,omitempty,omitzero"`
+	Type        Type     `json:"type,omitempty,omitzero" yaml:"type,omitempty,omitzero" toml:"type,omitempty,omitzero"`
+	Version     *Version `json:"version,omitempty,omitzero" yaml:"version,omitempty,omitzero" toml:"version,omitempty,omitzero"`
 }
 
 // Construct a new project.
@@ -25,40 +18,4 @@ func NewProject(name string, author *Author, description string, version *Versio
 	project.Description = description
 	project.Version = version
 	return project
-}
-
-// Lock the internal mutex of the project writing.
-func (project *Project) Lock() {
-	project.nameLock.Lock()
-	project.authorLock.Lock()
-	project.descriptionLock.Lock()
-	project.typeLock.Lock()
-	project.versionLock.Lock()
-}
-
-// Unlock the internal mutex of the project writing.
-func (project *Project) Unlock() {
-	project.nameLock.Unlock()
-	project.authorLock.Unlock()
-	project.descriptionLock.Unlock()
-	project.typeLock.Unlock()
-	project.versionLock.Unlock()
-}
-
-// Lock the internal mutex of the project for reading.
-func (project *Project) RLock() {
-	project.nameLock.RLock()
-	project.authorLock.RLock()
-	project.descriptionLock.RLock()
-	project.typeLock.RLock()
-	project.versionLock.RLock()
-}
-
-// Unlock the internal mutex of the project for reading.
-func (project *Project) RUnlock() {
-	project.nameLock.RUnlock()
-	project.authorLock.RUnlock()
-	project.descriptionLock.RUnlock()
-	project.typeLock.RUnlock()
-	project.versionLock.RUnlock()
 }
