ad09976fc288ab6548e2e46b7bfe37679ff899c4
Polshkrev <105235054+Polshkrev@users.noreply.github.com>
2026-09-18 13:35:03 -0400

    Update author.go

--- models/author.go
+++ models/author.go
@@ -1,6 +1,8 @@
 package models
 
 // Representation of an author of a project.
+//
+// Deprecated: Due to a move to c++, this will be deleted.
 type Author struct {
 	Name  string `json:"name,omitempty" yaml:"name,omitempty" toml:"name,omitempty"`
 	Email string `json:"email,omitempty" yaml:"email,omitempty" toml:"email,omitempty"`
@@ -8,6 +10,8 @@ type Author struct {
 
 // Construct a new project author.
 // Returns a new author with a given name and email.
+//
+// Deprecated: Due to a move to c++, this will be deleted.
 func NewAuthor(name, email string) *Author {
 	var author *Author = new(Author)
 	author.Name = name
